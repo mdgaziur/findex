9295507bc98ea42ca69c8d9df9898687533f99c4
Sergey A <murlakatamenka@disroot.org>
2021-11-07 20:23:54 +0300

    Query: Fix handling desktop entries without Name
    Addresses issue #6

--- src/query.rs
+++ src/query.rs
@@ -143,7 +143,11 @@ fn get_entries(dir: &str) -> Vec<AppInfo> {
         };
 
         let section = desktop_entry.section("Desktop Entry");
-        let name = section.attr("Name").unwrap();
+
+        let name: std::borrow::Cow<str> = match section.attr("Name") {
+            Some(name) => name.into(),
+            _ => app_path.file_name().unwrap_or_default().to_string_lossy(),
+        };
         let icon = section.attr("Icon").unwrap_or("applications-other");
         let exec = match section.attr("Exec") {
             Some(e) => {
