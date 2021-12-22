8b3eaf063b3b27ecc56ac335b8f50c807949f84b
mdgaziur <mdgaziurrahmannoor@gmail.com>
2021-12-22 19:29:45 +0600

    Daemon: Exit when current process is no longer owner of name

--- src/daemon/init.rs
+++ src/daemon/init.rs
@@ -1,11 +1,15 @@
-use dbus::blocking::Connection;
-use dbus_crossroads::Crossroads;
+use std::process::exit;
+use dbus::{arg, Message};
 use crate::daemon::paths::config::get_config;
+use dbus::blocking::Connection;
+use dbus::message::MatchRule;
+use dbus_crossroads::Crossroads;
 
 pub fn init_daemon() {
-    let con = Connection::new_session().expect("Failed to create new D-Bus session");
-    con.request_name("org.findex.daemon", false, false, true)
+    let con = Connection::new_session().expect("[Error] Failed to create new D-Bus session");
+    con.request_name("org.findex.daemon", true, true, false)
         .expect("[Error] Failed to request name on D-Bus");
+    let proxy = con.with_proxy("org.freedesktop.DBus", "/", std::time::Duration::from_millis(1000));
 
     let mut crossroads = Crossroads::new();
 
@@ -13,6 +17,23 @@ pub fn init_daemon() {
     crossroads.insert("/get_config", &[get_config_method], ());
 
     println!("[Info] Serving clients...");
-    crossroads.serve(&con)
-        .expect("[Error] Error occurred while serving clients");
+
+    use dbus::channel::MatchingReceiver;
+    con.start_receive(
+        dbus::message::MatchRule::new_method_call(),
+        Box::new(move |msg, conn| {
+            crossroads.handle_message(msg, conn).unwrap();
+            true
+        }),
+    );
+
+    loop {
+        let r: (String, ) = proxy.method_call("org.freedesktop.DBus", "GetNameOwner", ("org.findex.daemon",)).unwrap();
+        if con.unique_name().to_string() != r.0 {
+            println!("[Info] Daemon replaced with owner: {}", r.0);
+            break;
+        }
+        con.process(std::time::Duration::from_millis(1000))
+            .expect("[Error] Failed to process connection");
+    }
 }
