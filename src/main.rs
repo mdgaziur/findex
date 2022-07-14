206a0376933ffb75c11cfe87cffac11d4b5ed0d1
mdgaziur <mdgaziurrahmannoor@gmail.com>
2022-07-14 15:40:13 +0600

    Findex: only support X11

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
+
+
+
 use crate::config::FINDEX_CONFIG;
 use crate::gui::dialog::show_dialog;
 use crate::gui::GUI;
 use gtk::MessageType;
-use parking_lot::Mutex;
 
 mod app_list;
 mod config;
 mod gui;
 
 fn main() {
+    if std::env::var("XDG_SESSION_TYPE") != Ok(String::from("x11")) {
+        eprintln!("[Error] Only X11 is supported");
+        std::process::exit(1);
+    }
+
     println!("[INFO] Starting Findex...");
     gtk::init().expect("Failed to init GTK");
     if !FINDEX_CONFIG.error.is_empty() {
