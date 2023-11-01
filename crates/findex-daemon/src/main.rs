8803441564ade854864ac16fc5938a7f03255dcf
mdgaziur <mdgaziurrahmannoor@gmail.com>
2023-11-01 14:57:32 +0600

    Fix(#45): fix `ParseIntError` in `getpid`
    Thanks to @monomycelium for suggestions.

--- crates/findex-daemon/src/main.rs
+++ crates/findex-daemon/src/main.rs
@@ -1,37 +1,14 @@
 use daemonize::Daemonize;
 use inotify::{Inotify, WatchMask};
-use nix::libc::{pid_t, time_t};
+use nix::libc::time_t;
 use nix::time::ClockId;
 use shellexpand::tilde;
 use std::ffi::OsStr;
 use std::fs::{create_dir, File};
 use std::path::Path;
-use std::process::Command;
 use std::time::Duration;
 use subprocess::{ExitStatus, Popen, PopenConfig, Redirection};
-
-fn getpid(name: &str) -> Option<pid_t> {
-    let output = match Command::new("pidof").arg(name).output() {
-        Ok(o) => o,
-        Err(e) => {
-            eprintln!("[ERROR] Failed to run `pidof findex {name}`: {e}");
-            return None;
-        }
-    };
-
-    let pid_s = String::from_utf8(output.stdout)
-        .unwrap()
-        .split('\n')
-        .take(1)
-        .map(|s| s.to_string())
-        .collect::<String>();
-
-    if pid_s.is_empty() {
-        None
-    } else {
-        Some(pid_s.parse().unwrap())
-    }
-}
+use sysinfo::{ProcessRefreshKind, RefreshKind, System, SystemExt};
 
 fn findex_daemon(current_time: time_t) {
     fn spawn_findex(findex_output: File) -> Popen {
@@ -93,8 +70,12 @@ fn findex_daemon(current_time: time_t) {
 }
 
 fn main() {
-    if let Some(pid) = getpid("findex") {
-        eprintln!("[ERROR] Findex is already running with pid: {pid}");
+    if System::new_with_specifics(RefreshKind::new().with_processes(ProcessRefreshKind::new()))
+        .processes_by_exact_name("findex")
+        .count()
+        > 0
+    {
+        eprintln!("[ERROR] Findex is already running");
         eprintln!("[ERROR] Help: You may want to kill with `killall findex findex-daemon`");
 
         return;
